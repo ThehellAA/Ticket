a548cdcd0a6ec2488a253c46a8caf49cac20eac8
OUHAMMI Karim <48060593+ThehellAA@users.noreply.github.com>
2023-04-24 16:01:03 +0200

    Add files via upload

---
+++ ticket-business/src/main/java/org/example/demo/ticket/business/impl/ManagerFactory.java
@@ -0,0 +1,26 @@
+package org.example.demo.ticket.business.impl;
+
+import org.example.demo.ticket.business.contract.manager.ProjetManager;
+import org.example.demo.ticket.business.manager.TicketManager;
+
+public class ManagerFactory {
+
+    private ProjetManager projetManager;
+
+    public ProjetManager getProjetManager(){
+        return this.projetManager;
+    }
+    public void setProjetManager(ProjetManager projetManager) {
+        this.projetManager = projetManager;
+    }
+
+    private TicketManager ticketManager;
+    public TicketManager getTicketManager(){
+        return this.ticketManager;
+    }
+    public void setTicketManager(TicketManager ticketManager) {
+        this.ticketManager = ticketManager;
+    }
+
+    //...
+}
